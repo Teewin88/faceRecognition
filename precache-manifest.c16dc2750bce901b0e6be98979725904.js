12e8608d212e33689ddb858fc2a03eaad390f27a
An Trinh <qantri@gmail.com>
2020-02-12 12:52:25 +0100

    Updates

---
+++ precache-manifest.c16dc2750bce901b0e6be98979725904.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5f03eec4a86b1f8f4d9cc192d2cb7c",
+    "url": "/faceRecognition/index.html"
+  },
+  {
+    "revision": "e88c86ee90f4a5dd286e",
+    "url": "/faceRecognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "21d62b8f0ac70638ca12",
+    "url": "/faceRecognition/static/css/main.12eacb17.chunk.css"
+  },
+  {
+    "revision": "e88c86ee90f4a5dd286e",
+    "url": "/faceRecognition/static/js/2.1e1c5f4a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/faceRecognition/static/js/2.1e1c5f4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21d62b8f0ac70638ca12",
+    "url": "/faceRecognition/static/js/main.ce0c8231.chunk.js"
+  },
+  {
+    "revision": "cbafb8dc80aeafd104b3",
+    "url": "/faceRecognition/static/js/runtime-main.3d04077f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/faceRecognition/static/media/SEGA.12a3db8b.woff"
+  }
+]);
